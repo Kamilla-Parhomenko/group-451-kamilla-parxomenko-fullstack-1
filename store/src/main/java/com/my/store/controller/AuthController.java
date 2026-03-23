566581b557cd73bd413d34adc2553e169dc79ba1
Kamilla-Parhomenko <parhomenkokamilla@gmail.com>
2026-03-23 17:01:14 +0400

    2026 03 08 00 02 02

--- store/src/main/java/com/my/store/controller/AuthController.java
+++ store/src/main/java/com/my/store/controller/AuthController.java
@@ -1,16 +1,26 @@
 package com.my.store.controller;
 
 import com.my.store.dto.RegisterDto;
+import com.my.store.repository.UserRepository;
+import com.my.store.service.UserService;
 import jakarta.persistence.Column;
+import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
 @Controller
 @RequestMapping("/auth")
+@RequiredArgsConstructor
 public class AuthController {
+
+    private final UserService userService;
+    private final UserRepository userRepository;
 
     @GetMapping("/login")
     public String loginPage(){
@@ -24,7 +34,8 @@ public class AuthController {
     }
 
     @PostMapping("/register")
-    public String register(){
+    public String register(@ModelAttribute("registerDto") RegisterDto dto, BindingResult result, RedirectAttributes redirectAttributes, Model model){
+        userService.register(dto);
         return "auth/register";
     }
 }
